b867aa0c71f7352c9690cfbeb3af22f699fbcb12
the-swagga <144693488+the-swagga@users.noreply.github.com>
2025-11-21 21:19:33 +0000

    Add ASEAN

--- affiliation_dataset.py
+++ affiliation_dataset.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 dataset = pd.read_csv('country_economics_data.csv')
 dataset['Affiliation'] = "''"
-dataset.to_csv('mod_countries_economics_data.csv', index=False)
+dataset.to_csv('mod_country_economics_data.csv', index=False)
