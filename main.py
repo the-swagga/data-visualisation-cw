73c008663cfd7244efb87d7095319fdd0f943a82
the-swagga <144693488+the-swagga@users.noreply.github.com>
2025-11-24 21:20:45 +0000

    Changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import altair as alt
 import pandas as pd
 from vega_datasets import data
+import json
 
 # Following countries' Population and GDP manually added to the dataset (source: imf.org IMF Datamapper):
 # Czechia - ID:203 Population:10.9 GDP:383
@@ -13,15 +14,24 @@ from vega_datasets import data
 
 dataset = pd.read_csv('mod_country_economics_data.csv')
 dataset['GDP Per Capita'] = dataset['GDP'] / dataset['Population']
+dataset['Population Density'] = (dataset['Population'] / dataset['Area']) * 1000000
 dataset['ID'] = dataset['ID'].astype(int)
 
-countries = alt.topo_feature(data.world_110m.url, 'countries')
+global_south_regions = ["Northern Africa", "Southern Asia", "South-Eastern Asia", "Middle Africa", "Eastern Asia",
+                        "Western Africa", "Eastern Africa", "Central Asia", "Caribbean", "Western Asia",
+                        "Central America", "South America", "Southern Africa"]
+global_south_dataset = dataset[dataset["Subregion"].isin(global_south_regions)].copy()
+
+with open("world_110m.json", "r") as world_110m_json:
+    world_110m = json.load(world_110m_json)
+
+countries = alt.Data(values=world_110m, format=alt.DataFormat(type='topojson', feature='countries'))
 
 gdp_per_capita_map = (alt.Chart(countries).mark_geoshape(stroke='white', strokeWidth=0.25).encode(
     color=alt.Color('GDP Per Capita:Q', scale=alt.Scale(range=['lightblue', 'darkblue']), title='GDP Per Capita ($B)', legend=alt.Legend(orient='right')))
     .transform_lookup(
         lookup='id',
-        from_=alt.LookupData(data=dataset, key='ID', fields=['GDP Per Capita']))
+        from_=alt.LookupData(data=global_south_dataset, key='ID', fields=['GDP Per Capita']))
     .project(
         type='naturalEarth1')
     .properties(
@@ -29,25 +39,25 @@ gdp_per_capita_map = (alt.Chart(countries).mark_geoshape(stroke='white', strokeW
         height=400)
 )
 
-gdp_pop_scatter = alt.Chart(dataset).mark_circle(clip=True).encode(
-    x=alt.X('Population:Q', title='Population (M)', scale=alt.Scale(type='log'), axis=alt.Axis(grid=False)),
-    y=alt.Y('GDP:Q', title='GDP ($B)', scale=alt.Scale(domain=[-10, 5000]), axis=alt.Axis(grid=False)),
-    color=alt.Color('Region:N', scale=alt.Scale(domain=['Africa', 'Asia', 'Oceania'], range=['Orange', 'Blue', 'Red']), legend=alt.Legend(title='Region', orient='bottom')),
-    size=alt.Size('GDP Per Capita:Q', scale=alt.Scale(range=[50,500]), legend=None)).properties(
+gdp_pop_scatter = alt.Chart(global_south_dataset).mark_circle(clip=True).encode(
+    x=alt.X('GDP Per Capita:Q', title='GDP Per Capita', scale=alt.Scale(type='log'), axis=alt.Axis(grid=False)),
+    y=alt.Y('Jobless Rate:Q', title='Unemployment Rate (%)', scale=alt.Scale(domain=[0, 50]), axis=alt.Axis(grid=False)),
+    color=alt.Color('Region:N', scale=alt.Scale(domain=['Africa', 'Asia', 'Americas'], range=['Blue', 'Red', 'Orange']), legend=alt.Legend(title='Region', orient='bottom')),
+    size=alt.Size('GDP:Q', scale=alt.Scale(range=[75, 750]), legend=None)).properties(
         width = 1000,
         height = 400
 )
 
-continent_bar = alt.Chart(dataset).transform_filter(
+continent_bar = alt.Chart(global_south_dataset).transform_filter(
     'datum.Subregion != null'
 ).mark_bar().encode(
     x=alt.X('mean(GDP Growth):Q', title='Average GDP Growth (%)'),
-    y=alt.Y('Subregion:N', title='', sort='x'),
-    color=alt.Color('Region:N', legend=None)
+    y=alt.Y('Subregion:N', title='', sort='-x'),
+    color=alt.Color('Region:N', scale=alt.Scale(domain=['Africa', 'Asia', 'Americas'], range=['Blue', 'Red', 'Orange']), legend=None)
 ).properties(
     width=600,
     height=400,
-    title="Average GDP Growth by Region"
+    title="GDP Growth in the Global South"
 )
 
 dashboard = (gdp_per_capita_map | continent_bar) & (gdp_pop_scatter)
